dfa4b7f6609e5a573410b73acb8f943491cd7c6f
Aleksandr Korovin <Aleksandr Korovin>
2016-08-16 17:54:28 +0200

    Adding property array to store users in UsersPage Component

--- ionic2/app/pages/users/users.ts
+++ ionic2/app/pages/users/users.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import {GithubUsers} from '../../providers/github-users/github-users';
+import {User} from "../../models/user";
 /*
   Generated class for the UsersPage page.
 
@@ -14,15 +15,15 @@ import {GithubUsers} from '../../providers/github-users/github-users';
   providers: [GithubUsers]
 })
 export class UsersPage {
+  // Declare users as an array of User model
+  users: User[];
+
   // Inject GithubUsers in the constructor of our page component (Dependency Injection
   // of github users)
   constructor(private navCtrl: NavController, githubUsers: GithubUsers) {
     // Test whether github provider returns data
     githubUsers
       .load()
-      .then((users) => {
-        // Log returned github users
-        console.log(users);
-      });
+      .then((users) => this.users = users);
   }
 }
