bf4f5d61bb557d75436f4809c79180547c7a3c5f
Aleksandr Korovin <Aleksandr Korovin>
2016-08-16 16:58:17 +0200

    Some comments about app.ts and html

--- ionic2/app/app.ts
+++ ionic2/app/app.ts
@@ -4,7 +4,9 @@ import {StatusBar} from 'ionic-native';
 import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
 import {ListPage} from './pages/list/list';
 
-
+/**
+ * Root app component
+ */
 @Component({
   templateUrl: 'build/app.html'
 })
